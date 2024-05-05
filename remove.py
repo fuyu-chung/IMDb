42b2d9e1bb8a74fed39c1e21741495352c764436
鍾馥羽 <fuyuchung@gmail.com>
2024-05-06 01:27:03 +0800

    remove

--- remove.py
+++ remove.py
@@ -5,24 +5,25 @@ import pandas as pd
 # file1.drop(['birthYear', 'deathYear'], axis = 1, inplace = True) #remove birthYear, deathYear column
 # file1.to_csv("new.name.basics.csv", index = False) # row output
 
-# # # title.akas.csv
+# title.akas.csv
 # file2 = pd.read_csv("title.akas.csv") # read title.akas.csv
-# file2.drop(['types', 'attributes', 'isOriginalTitle', 'language'], axis = 1, inplace = True) #remove types, attributes, isOriginalTitle, language column
-# file2.drop(file2.loc[file2['ordering'] != 2].index, inplace = True) #remove ordering != 2 row
-# file2.drop(['ordering'], axis = 1, inplace = True) #remove ordering column
-# file2.drop(file2.loc[file2['region'].isin(['\\N'])].index, inplace = True) #remove region == '\\N' row
+# file2.drop(['title', 'types', 'attributes', 'isOriginalTitle', 'language'], axis = 1, inplace = True) #remove title, types, attributes, isOriginalTitle, language column
+# file2.drop(file2.loc[file2['ordering'] != 2].index, inplace = True) # remove ordering != 2 row
+# file2.drop(['ordering'], axis = 1, inplace = True) # remove ordering column
+# file2.drop(file2.loc[file2['region'].isin(['\\N'])].index, inplace = True) # remove region == '\\N' row
+# file2.rename(columns={"titleId": "tconst"}, inplace = True) # rename titleId to tconst
 # file2.to_csv("new.title.akas.csv", index = False) # row output
 
 # # title.basic.csv
-# file3 = pd.read_csv("title.basics.csv") # read title.basics.csv
-# file3.drop(['originalTitle', 'isAdult', 'endYear'], axis = 1, inplace = True) # remove originalTitle, isAdult, endYear column
-# file3.drop(file3.loc[file3['startYear'] < '2000'].index, inplace = True) # remove startYear < 2000 row
-# file3.drop(file3.loc[file3['startYear'].isin(['\\N'])].index, inplace = True) # remove startYear == '\\N' row
-# file3.drop(file3.loc[file3['runtimeMinutes'].isin(['\\N'])].index, inplace = True) # remove runtimeMinutes == '\\N' row
-# file3.drop(file3.loc[file3['genres'].isin(['\\N'])].index, inplace = True) # remove genres == '\\N' row
-# file3.drop(file3.loc[~file3['titleType'].isin(['movie'])].index, inplace = True) # remove titleType != 'movie' row
-# file3.drop(['titleType'], axis = 1, inplace = True) # remove 'titleType' column
-# file3.to_csv("new.title.basics.csv", index = False) # output
+file3 = pd.read_csv("title.basics.csv") # read title.basics.csv
+file3.drop(['originalTitle', 'isAdult', 'endYear'], axis = 1, inplace = True) # remove originalTitle, isAdult, endYear column
+file3.drop(file3.loc[file3['startYear'] < '2000'].index, inplace = True) # remove startYear < 2000 row
+file3.drop(file3.loc[file3['startYear'].isin(['\\N'])].index, inplace = True) # remove startYear == '\\N' row
+file3.drop(file3.loc[file3['runtimeMinutes'].isin(['\\N'])].index, inplace = True) # remove runtimeMinutes == '\\N' row
+file3.drop(file3.loc[file3['genres'].isin(['\\N'])].index, inplace = True) # remove genres == '\\N' row
+file3.drop(~file3.loc[file3['titleType'].isin(['movie'])].index, inplace = True) # remove titleType != 'movie' row
+file3.drop(['titleType'], axis = 1, inplace = True) # remove 'titleType' column
+file3.to_csv("new.title.basics.csv", index = False) # output
 
 # # title.crew.csv
 # file4 = pd.read_csv("title.crew.csv") # read title.crew.csv
