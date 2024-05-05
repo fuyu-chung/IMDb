646f31a8b5c07dc8a2c33835e0a2c1f965c5329a
鍾馥羽 <fuyuchung@gmail.com>
2024-05-05 21:36:19 +0800

    remove

--- remove.py
+++ remove.py
@@ -10,29 +10,29 @@ import pandas as pd
 # file2.drop(['types', 'attributes', 'isOriginalTitle', 'language'], axis = 1, inplace = True) #remove types, attributes, isOriginalTitle, language column
 # file2.drop(file2.loc[file2['ordering'] != 2].index, inplace = True) #remove ordering != 2 row
 # file2.drop(['ordering'], axis = 1, inplace = True) #remove ordering column
-# file2.drop(file2.loc[file2['region'] == '\\N'].index, inplace = True) #remove region == '\\N' row
+# file2.drop(file2.loc[file2['region'].isin(['\\N'])].index, inplace = True) #remove region == '\\N' row
 # file2.to_csv("new.title.akas.csv", index = False) # row output
 
 # # title.basic.csv
-file3 = pd.read_csv("title.basics.csv") # read title.basics.csv
-file3.drop(['originalTitle', 'isAdult', 'endYear'], axis = 1, inplace = True) # remove originalTitle, isAdult, endYear column
-file3.drop(file3.loc[file3['startYear'] < '2000'].index, inplace = True) # remove startYear < 2000 row
-file3.drop(file3.loc[file3['startYear'] == '\\N'].index, inplace = True) # remove startYear == '\\N' row
-file3.drop(file3.loc[file3['runtimeMinutes'] == '\\N'].index, inplace = True) # remove runtimeMinutes == '\\N' row
-file3.drop(file3.loc[file3['genres'] == '\\N'].index, inplace = True) # remove genres == '\\N' row
-file3.drop(file3.loc[file3['titleType'] != 'movie'].index, inplace = True) # remove titleType != 'movie' row
-file3.drop(['titleType'], axis = 1, inplace = True) # remove 'titleType' column
-file3.to_csv("new.title.basics.csv", index = False) # output
+# file3 = pd.read_csv("title.basics.csv") # read title.basics.csv
+# file3.drop(['originalTitle', 'isAdult', 'endYear'], axis = 1, inplace = True) # remove originalTitle, isAdult, endYear column
+# file3.drop(file3.loc[file3['startYear'] < '2000'].index, inplace = True) # remove startYear < 2000 row
+# file3.drop(file3.loc[file3['startYear'].isin(['\\N'])].index, inplace = True) # remove startYear == '\\N' row
+# file3.drop(file3.loc[file3['runtimeMinutes'].isin(['\\N'])].index, inplace = True) # remove runtimeMinutes == '\\N' row
+# file3.drop(file3.loc[file3['genres'].isin(['\\N'])].index, inplace = True) # remove genres == '\\N' row
+# file3.drop(file3.loc[~file3['titleType'].isin(['movie'])].index, inplace = True) # remove titleType != 'movie' row
+# file3.drop(['titleType'], axis = 1, inplace = True) # remove 'titleType' column
+# file3.to_csv("new.title.basics.csv", index = False) # output
 
 # # title.crew.csv
 # file4 = pd.read_csv("title.crew.csv") # read title.crew.csv
-# file4.drop(file4.loc[file4['writers'] == '\\N'].index, inplace = True) # remove writers == '\\N' row
+# file4.drop(file4.loc[file4['writers'].isin(['\\N'])].index, inplace = True) # remove writers == '\\N' row
 # file4.to_csv("new.title.crew.csv", index = False) # output
 
 # # title.episode.csv
 # file5 = pd.read_csv("title.episode.csv") # read title.episode.csv
-# file5.drop(file5.loc[file5['seasonNumber'] == '\\N'].index, inplace = True) # remove seasonNumber == '\\N' row
-# file5.drop(file5.loc[file5['episodeNumber'] == '\\N'].index, inplace = True) # remove episodeNumber == '\\N' row
+# file5.drop(file5.loc[file5['seasonNumber'].isin(['\\N'])].index, inplace = True) # remove seasonNumber == '\\N' row
+# file5.drop(file5.loc[file5['episodeNumber'].isin(['\\N'])].index, inplace = True) # remove episodeNumber == '\\N' row
 # file5.to_csv("new.title.episode.csv", index = False) # output
 
 # # title.principals.csv
@@ -43,7 +43,7 @@ file3.to_csv("new.title.basics.csv", index = False) # output
 
 # # title.ratings.csv
 # file7 = pd.read_csv("title.ratings.csv") # read title.ratings.csv
-# # print(file7.loc[:, 'numVotes'].mean()) # print numVote average
-# file7.drop(file7.loc[file7['numVotes'] < 500].index, inplace = True) #remove numVotes < 1000 row
+# print(file7.loc[:, 'numVotes'].mean()) # print numVote average
+# file7.drop(file7.loc[file7['numVotes'] < 500].index, inplace = True) #remove numVotes < 500 row
 # file7.to_csv("new.title.ratings.csv", index = False) # output
 
