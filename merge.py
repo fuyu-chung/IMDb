eff18cd09fb5d5ea1c9bbc548f54b3a74725140d
鍾馥羽 <fuyuchung@gmail.com>
2024-05-11 23:50:07 +0800

    Merged new akas/basics/crews/ratings files

--- merge.py
+++ merge.py
@@ -4,7 +4,7 @@ import pandas as pd
 file2 = pd.read_csv("new.title.akas.csv") # read new title.akas.csv
 file3 = pd.read_csv("new.title.basics.csv") # read new title.basics.csv
 file4 = pd.read_csv("new.title.crew.csv") # read new title.crew.csv
-file5 = pd.read_csv("new.title.episode.csv") # read new title.episode.csv
+# file5 = pd.read_csv("new.title.episode.csv") # read new title.episode.csv
 # file6 = pd.read_csv("new.title.principals.csv") # read new title.principals.csv
 file7 = pd.read_csv("new.title.ratings.csv") # read new title.ratings.csv
 
