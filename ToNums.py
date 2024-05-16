00d50256308e7b36bf2329de74bebe6668d39b10
Felicia572 <feliciachan572@gmail.com>
2024-05-16 13:23:50 +0800

    update py

---
+++ ToNums.py
@@ -0,0 +1,158 @@
+import pandas as pd
+import os
+
+file1 = pd.read_csv("new_csv\new.akas_basics_crew_ratings.csv") # read akas_basics_crew_ratings.csv
+file2 = pd.read_csv("new_csv\new.name.basics.csv") # read new.name.basics.csv
+file3 = pd.read_csv("new_csv\new.title.principals.csv") # read new.title.principals.csv
+count = 0
+
+"""# **Region**"""
+
+# 統計地區及次數，放入 region_rate
+region_rate = {}
+
+for i in range(len(file1)):
+    region = file1.iloc[i]['region']
+    rating = file1.iloc[i]['averageRating']
+    if region in region_rate:
+        region_rate[region]['total'] += rating
+        region_rate[region]['count'] += 1
+    else:
+        region_rate[region] = {'total': rating, 'count': 1}
+
+    # if count == 10:
+    #     break
+    # count += 1
+
+# 計算各地區的平均評分
+avg_ratings = {}
+for region, values in region_rate.items():
+    average_rating = values['total'] / values['count']
+    avg_ratings[region] = average_rating
+
+# 排序
+# sorted_regions = sorted(avg_ratings.items(), key=lambda x: x[1], reverse=True)
+# for region, average_rating in sorted_regions:
+    # print("Region:", region, "Average Rating:", average_rating)
+
+# 更換 region
+file1['region'] = file1['region'].map(avg_ratings)
+# file1.to_csv('new_file.csv', index=False)
+
+"""# **Genres**"""
+
+# 統計 gemres 次數，放入 genres_rate
+genres_rate = {}
+for i in range(len(file1)):
+    genres = file1.iloc[i]['genres'].split(',')
+    rating = file1.iloc[i]['averageRating']
+    for genre in genres:
+        genre = genre.strip()
+        if genre in genres_rate:
+            genres_rate[genre]['total'] += rating
+            genres_rate[genre]['count'] += 1
+        else:
+            genres_rate[genre] = {'total': rating, 'count': 1}
+    # if count == 10:
+    #     break
+    # count += 1
+# 計算每個 genre 的平均評分
+avg_ratings = {}
+for genre, values in genres_rate.items():
+    avg_rating = values['total'] / values['count']
+    avg_ratings[genre] = avg_rating
+
+# 每部電影平均評分
+avg_movie = []
+for i in range(len(file1)):
+    genres = file1.iloc[i]['genres'].split(',')
+    avg_rating_sum = 0
+    for genre in genres:
+        avg_rating_sum += avg_ratings[genre.strip()] # 平均和
+    avg_rating = avg_rating_sum / len(genres)
+    avg_movie.append(avg_rating)
+
+# 更換 genres
+file1['genres'] = avg_movie
+
+len(genres_rate)
+
+"""# **Directors and Actor**
+
+## **電影對應的評分存在 tconst_rate (字典)**
+"""
+
+tconst_rate = {}
+for i in range(len(file1)):
+    tconst_rate[file1.iloc[i]['tconst']] = file1.iloc[i]['averageRating']
+
+"""## **File2**
+
+### 讀取 file2 每一列，並新增工作人員的 rate 存在新的 csv
+"""
+
+for i in range(len(file2)):
+    sum = 0
+    kftitles = file2.iloc[i]['knownForTitles'].split(',')
+    for movie in kftitles:
+        if movie in tconst_rate:
+            sum += tconst_rate[movie]
+        else:
+            print(file2.iloc[i]['nconst'],movie)
+
+    avg_rating = sum / len(kftitles)
+    file2.at[i, 'rate'] = avg_rating
+    # print("Average Rating for row", file2.iloc[i]['nconst'], ":", avg_rating)
+    # rate_list.append(avg_rating)
+    # if count == 10:
+    #     break
+    # count += 1
+
+# 如果 Directors_Actor_Writer.csv 存在，則刪掉
+if os.path.exists('new_csv\Directors_Actor_Writer.csv'):
+    os.remove('new_csv\Directors_Actor_Writer.csv')
+
+# 保存新的 CSV
+file2.to_csv('new_csv\Directors_Actor_Writer.csv', index=False)
+
+"""### 工作人員對應的評分存在 nconst_rate (字典)"""
+
+nconst_rate = {}
+for i in range(len(file2)):
+    nconst_rate[file2.iloc[i]['nconst']] = file2.iloc[i]['rate']
+
+for i in range(len(file1)):
+    directors = file1.iloc[i]['directors'].split(',')
+    sum = 0
+    for director in directors:
+        if director in nconst_rate:
+            sum += nconst_rate[director]
+    avg_rating = sum / len(directors)
+    file1.at[i, 'directors'] = avg_rating
+
+    # print(file1.iloc[i]['tconst'],sum)
+
+    # if count == 10:
+    #     break
+    # count += 1
+
+"""## **Writer**"""
+
+for i in range(len(file1)):
+    writers = file1.iloc[i]['writers'].split(',')
+    sum = 0
+    for writer in writers:
+        if writer in nconst_rate:
+            sum += nconst_rate[writer]
+    avg_rating = sum / len(writers)
+    file1.at[i, 'writers'] = avg_rating
+
+"""## **存檔**"""
+
+# 如果 Directors_Actor_Writer.csv 存在，則刪掉
+if os.path.exists('new_csv\vector.csv'):
+    os.remove('new_csv\vector.csv')
+
+# 保存新的 CSV
+file1.to_csv('new_csv\vector.csv', index=False)
+
---
+++ train.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import GridSearchCV
+import matplotlib.pyplot as plt
+import os
+
+data = pd.read_csv("new_csv\vector.csv")
+
+# 切割 train data 與 test data
+train_data = data[data['startYear'] < 2020]
+test_data = data[data['startYear'] >= 2020]
+
+# train data
+X_train = train_data[['region', 'startYear', 'runtimeMinutes', 'genres', 'directors', 'writers', 'numVotes']]
+y_train = train_data['averageRating']
+
+# test data
+X_test = test_data[['region', 'startYear', 'runtimeMinutes', 'genres', 'directors', 'writers', 'numVotes']]
+y_test = test_data['averageRating']
+tconst = test_data['tconst']
+primaryTitle = test_data['primaryTitle']
+
+"""## 訓練模型"""
+
+# 建立隨機森林
+model = RandomForestRegressor(n_estimators=1000, random_state=42)
+
+# 訓練
+model.fit(X_train, y_train)
+
+# 預測
+y_pred = model.predict(X_test)
+
+"""### 檢查模型是否良好"""
+
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+
+"""## 畫圖"""
+
+plt.figure(figsize=(8, 6))
+plt.scatter(y_test, y_pred, color='blue',s=5)
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linestyle='--')
+plt.xlabel('Actual')
+plt.ylabel('Predicted')
+plt.savefig('new_csv\result.png')
+plt.show()
+
+"""## 建立 csv"""
+
+# 建立包含 actual、predict、tconst和  primaryTitle 的DataFrame
+results = pd.DataFrame({'tconst': tconst, 'primaryTitle': primaryTitle, 'Actual': y_test, 'Predicted': y_pred})
+
+# 印前十行
+print(results.head(10))
+
+if os.path.exists('new_csv\result.csv'):
+    os.remove('new_csv\result.csv')
+
+results.to_csv('new_csv\result.csv', index=False)
